cccc5d34a4820f5f13fb5875ffd921b4fa4cd145
Kathy Nguyen <kathyvng@gmail.com>
2018-06-20 04:13:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
-countdown_output = 10
 
 def countdown(countdown_output)
-  while countdown_output > 10
+  while countdown_output > 0
     countdown_output -= 1
     puts "#{countdown_output} SECOND(S)!"
   end
@@ -11,5 +10,10 @@ end
 countdown(countdown_output)
 
 def countdown_with_sleep
+  while countdown_output > 0
+    countdown_output -= 1
+    puts "#{countdown_output} SECOND(S)!"
   sleep 5
+  end
+  return "HAPPY NEW YEAR!"
 end
