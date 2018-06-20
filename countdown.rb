4e5acd9cd12a4c4d099468cc4d703edaa4cdfe32
Kathy Nguyen <kathyvng@gmail.com>
2018-06-20 02:08:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@ countdown_output = 10
 
 def countdown(countdown_output)
   while countdown_output < 10
-    puts "#{number} SECOND(S)!"
-    countdown_output -= 1  
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
   end
     return "HAPPY NEW YEAR!"
 end
