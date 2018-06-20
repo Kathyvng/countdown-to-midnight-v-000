54c2a5f9c4bdaa9ad4f70914211da12d502efa7b
Kathy Nguyen <kathyvng@gmail.com>
2018-06-20 04:16:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(countdown_output)
     countdown_output -= 1
     puts "#{countdown_output} SECOND(S)!"
   end
-    return "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
 
 
@@ -14,5 +14,5 @@ def countdown_with_sleep(countdown_output)
     puts "#{countdown_output} SECOND(S)!"
   sleep 5
   end
-  return "HAPPY NEW YEAR!"
+   "HAPPY NEW YEAR!"
 end
